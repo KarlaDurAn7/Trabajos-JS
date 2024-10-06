424c28dca90558283d5c8c0ee47b510747da0850
KarlaDurAn7 <kcubiasduran@gmail.com>
2024-10-05 20:25:52 -0600

    Add files via upload

---
+++ Calculadora-7/ayuda.js
@@ -0,0 +1,60 @@
+function suma(){
+    let numero1 = +document.getElementById('numero1').value;   
+    let numero2 = +document.getElementById('numero2').value;     
+    let display = document.getElementById('salida'); 
+    display.textContent = numero1 + numero2;    
+}
+function resta() {
+    let numero1 = +document.getElementById('numero1').value; 
+    let numero2 = +document.getElementById('numero2').value;  
+    let display = document.getElementById('salida');          
+    display.textContent = numero1 - numero2;  
+}
+
+function multiplicacion() {
+    let numero1 = +document.getElementById('numero1').value;
+    let numero2 = +document.getElementById('numero2').value;
+    let display = document.getElementById('salida');
+    display.textContent =  numero1 * numero2;
+}
+
+function division() {
+    let numero1 = +document.getElementById('numero1').value;
+    let numero2 = +document.getElementById('numero2').value;
+    let display = document.getElementById('salida');
+
+    if (numero2 !== 0) {
+        display.textContent = numero1 / numero2;
+    } else {
+        display.textContent = 'Error';
+         }
+    }
+
+    function potencia() {
+        let numero1 = +document.getElementById('numero1').value;
+        let numero2 = +document.getElementById('numero2').value;
+        let display = document.getElementById('salida');
+        display.textContent = Math.pow(numero1, numero2);
+    }
+
+    function raiz() {
+        let numero1 = +document.getElementById('numero1').value;
+        let display = document.getElementById('salida');
+
+        if (numero1 >= 0) {
+            display.textContent = Math.sqrt(numero1);
+        } 
+        else {
+            display.textContent = 'Error';
+        }
+       }
+
+    function borrar(){
+        document.getElementById('numero1').value = ' ';
+        document.getElementById('numero2').value = ' ';
+        
+        let display = document.getElementById('salida');
+        display.textContent = ' ';
+       }
+
+
